e108666e2c599ab1dcfbb012fcd415e1203b9a91
Technopreneur Milind <milind.technopreneur@gmail.com>
2023-09-06 17:57:38 +0530

    Commit

--- src/main/java/com/technopreneur/milind/ResilientController.java
+++ src/main/java/com/technopreneur/milind/ResilientController.java
@@ -5,25 +5,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 @RequestMapping("/resilient/api")
 public class ResilientController {
 	
 	@Autowired
-	private ResilientCallService resilientCallService;
+	private MyService myService;
 	
 	@GetMapping("/value1")
+	@Bulkhead(name="bulkheadApi")
 	@CircuitBreaker(name = "CircuitBreakerService")
 	public String getValue1() {
-		return resilientCallService.getValue1();
+		return myService.getValue1();
 		
 	}
 	
 	@GetMapping("/value2")
+	@RateLimiter(name = "rateLimiterApi")
+	@CircuitBreaker(name = "CircuitBreakerService")
 	public String getValue2() {
-		return resilientCallService.getValue2();
+		return myService.getValue2();
 	}
 
 }
